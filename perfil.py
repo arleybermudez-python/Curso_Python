ef9ff550637bdcd122e6fa5227ceb682b7b407bc
Arley Bermudez <arleybermudez@gmail.com>
2025-12-26 20:37:13 -0500

    Listo

---
+++ perfil.py
@@ -0,0 +1,8 @@
+#Este pequeño script pide datos a un usuario
+nombre = input("Indique su nombre:")
+objetivo = input("Cual es tu objetivo con Python:")
+
+print("PERFIL DEL PROGRAMADOR")
+print("Nombre: ", nombre)
+print("Tu meta es:", objetivo)
+print("Vamos", nombre, "que nada te detenga")
