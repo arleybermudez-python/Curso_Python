f1eaa12f8805144de33fa24dc32ee77a55dbef12
Arley Bermudez <arleybermudez@gmail.com>
2025-12-30 17:55:00 -0500

    Listo

---
+++ practicas.py
@@ -0,0 +1,15 @@
+# Archivo de practica
+# Variables
+# Tipos de Variables
+numero = int(input("Escribe un numero:"))
+print(numero)
+# Estoy pidiendo al usuario que ingrese un numero, para eso se usa el input, lo que esta
+# ingresando es texto de forma vulgar, pero con el int lo convierte en integer que significa
+# entero.
+# Los numeros flotantes o decimales por norma en Python se escriben con punto y no con coma
+decimal = float(input("Escribe un numero decimal:"))
+print(decimal)
+# Cadenas de texto, si o si siempre van en comillas, se puede guardar cualquier letra,
+# palabra, frases e incluso numeros
+nombre = str(input("Ingresa tu nombre:"))
+print(nombre)
