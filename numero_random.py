2c263e464678c955ad2cb534d07b82846c6e12ac
Arley Bermudez <arleybermudez@gmail.com>
2025-12-31 15:11:16 -0500

    Listo

---
+++ numero_random.py
@@ -0,0 +1,6 @@
+#Generador de numeros aleatorios del 1 al 100
+
+import random
+print("---- Generador de numeros aleatorios ----")
+numero_secreto = random.randint(1, 100)
+print(f"El numero es:{numero_secreto}")
