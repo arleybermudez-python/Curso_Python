37458c08ba646793a3460a289df2630ea9c0b754
Arley Bermudez <arleybermudez@gmail.com>
2026-01-07 00:35:31 -0500

    Listo

---
+++ lista.py
@@ -0,0 +1,27 @@
+#Vamos a trabajar listas
+variable_1 = "variable"
+numeros_lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print (numeros_lista)
+
+# Para imprimir un valor dentro de la lista 
+print(numeros_lista[3])
+
+#len() cuenta el numero total de elementos de una lista
+total = len(numeros_lista)
+
+print(f"El número total de elementos en la lista es", total)
+
+#invertir el orden de la lista
+numeros_lista.reverse()
+
+print(numeros_lista)
+
+#sum()
+
+sumatoria_lista = sum(numeros_lista)
+print(sumatoria_lista)
+
+
+
+
+
